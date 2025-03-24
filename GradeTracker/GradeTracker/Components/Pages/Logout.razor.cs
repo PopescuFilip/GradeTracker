2cd9e65e427011e3ac25c62a4dec592dba9ab5a8
FilipPopescu <filippopescu18@gmail.com>
2025-03-24 11:37:50 +0200

    SCRUM-14 Added logout and AccessDenied pages

---
+++ GradeTracker/GradeTracker/Components/Pages/Logout.razor.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Components;
+
+namespace GradeTracker.Components.Pages;
+
+public partial class Logout
+{
+    [CascadingParameter]
+    HttpContext HttpContext { get; set; }
+
+    [Inject]
+    NavigationManager NavigationManager { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        if (HttpContext.User.Identity.IsAuthenticated)
+        {
+            await HttpContext.SignOutAsync();
+            NavigationManager.NavigateTo("/logout", true);
+        }
+    }
+}
