2226cf0d2aad62a8682ee939682a08134f8d5e4a
FilipPopescu <filippopescu18@gmail.com>
2025-03-24 10:38:25 +0200

    SCRUM-14 created login page and LoginViewModel

---
+++ GradeTracker/GradeTracker/Components/Pages/Login.razor.cs
@@ -0,0 +1,55 @@
+using GradeTracker.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components;
+using System.Security.Claims;
+
+namespace GradeTracker.Components.Pages;
+
+public partial class Login
+{
+    [CascadingParameter]
+    public HttpContext HttpContext { get; set; }
+
+
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    private bool InvalidUsernameOrPassword { get; set; }
+
+    [SupplyParameterFromForm]
+    public LoginViewModel Model { get; set; } = new();
+
+    public async Task Authenticate()
+    {
+        var username = Model.Username;
+        var password = Model.Password;
+        //var userId = await UserService.Check(username, password);
+        var userId = "Guid.NewGuid()";
+
+        if (!Guid.TryParse(userId, out _))
+        {
+            InvalidUsernameOrPassword = true;
+            return;
+        }
+
+        await SignIn(username);
+
+        Console.WriteLine("succesful login");
+    }
+
+    private async Task SignIn(string username)
+    {
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.Name, username),
+            new(ClaimTypes.Role, "user")
+        };
+
+        var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        var principal = new ClaimsPrincipal(identity);
+        await HttpContext.SignInAsync(principal);
+        NavigationManager.NavigateTo("/");
+    }
+}
