32caaa6e2e13b9540324264875891a22f51f0307
FilipPopescu <filippopescu18@gmail.com>
2025-03-27 19:20:27 +0200

    SCRUM-8 Added code for populating Database if it does not contain basic entities and setup Program.cs DI

--- GradeTracker/GradeTrackerWebAPI/Program.cs
+++ GradeTracker/GradeTrackerWebAPI/Program.cs
@@ -1,4 +1,7 @@
 using GradeTrackerWebAPI.Data;
+using GradeTrackerWebAPI.Models;
+using GradeTrackerWebAPI.Services;
+using GradeTrackerWebAPI.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,10 +13,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IEntityService<TeacherEntity>, EntityService<TeacherEntity>>();
+builder.Services.AddScoped<IEntityService<StudentEntity>, EntityService<StudentEntity>>();
+builder.Services.AddScoped<IEntityService<ClassEntity>, EntityService<ClassEntity>>();
+builder.Services.AddScoped<IEntityService<SubjectEntity>, EntityService<SubjectEntity>>();
+builder.Services.AddScoped<IUserService, UserService>();
+
 builder.Services.AddDbContext<GradeTrackerContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("GradeTrackerDB")));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    await PopulateDb(scope);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -29,3 +43,50 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static async Task PopulateDb(IServiceScope scope)
+{
+    var classService = scope.ServiceProvider.GetRequiredService<IEntityService<ClassEntity>>();
+    var classes = await classService.GetAll();
+    if (classes == null || classes.Count == 0)
+    {
+        await classService.Create(new ClassEntity() { Name = "3C"});
+    }
+
+    var subjectService = scope.ServiceProvider.GetRequiredService<IEntityService<SubjectEntity>>();
+    var subjects = await subjectService.GetAll();
+    if (subjects == null || subjects.Count == 0)
+    {
+        await subjectService.Create(new SubjectEntity() { Name =  "Math" });
+    }
+
+    var teacherService = scope.ServiceProvider.GetRequiredService<IEntityService<TeacherEntity>>();
+    var teachers = await teacherService.GetAll();
+    if (teachers == null || teachers.Count == 0)
+    {
+        var newTeacher = new TeacherEntity()
+        {
+            FirstName = "Gerald",
+            LastName = "Geraldson",
+            Username = "teacher",
+            Password = "pass",
+            Subject = (await subjectService.GetAll()).First()
+        };
+        await teacherService.Create(newTeacher);
+    }
+
+    var studentService = scope.ServiceProvider.GetRequiredService<IEntityService<StudentEntity>>();
+    var students = await studentService.GetAll();
+    if (students == null || students.Count == 0)
+    {
+        var newStudent = new StudentEntity()
+        {
+            FirstName = "John",
+            LastName = "Maical",
+            Username = "student",
+            Password = "pass",
+            Class = (await classService.GetAll()).First()
+        };
+        await studentService.Create(newStudent);
+    }
+}
