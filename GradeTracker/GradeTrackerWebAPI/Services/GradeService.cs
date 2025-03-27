04d2b200ba5b767e28e21b997d3c4747e05d23b6
FilipPopescu <filippopescu18@gmail.com>
2025-03-27 22:16:35 +0200

    SCRUM-8 Created Grade and User services

---
+++ GradeTracker/GradeTrackerWebAPI/Services/GradeService.cs
@@ -0,0 +1,26 @@
+﻿using GradeTrackerWebAPI.Data;
+using GradeTrackerWebAPI.Helpers;
+using GradeTrackerWebAPI.Models;
+using GradeTrackerWebAPI.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GradeTrackerWebAPI.Services;
+
+public class GradeService(GradeTrackerContext context) : EntityService<GradeEntity>(context), IEntityService<GradeEntity>
+{
+    public async override Task<List<GradeEntity>> GetAll(bool includeAllProperties = false)
+    {
+        if (!includeAllProperties)
+            return await base.GetAll(includeAllProperties);
+
+        return await _context.Set<GradeEntity>()
+            .IncludeAll()
+            .ToListAsync();
+    }
+
+    public override async Task<GradeEntity?> Get(int id)
+        => await _context.Set<GradeEntity>()
+        .Where(x => x.Id == id)
+        .IncludeAll()
+        .FirstOrDefaultAsync();
+}
---
+++ GradeTracker/GradeTrackerWebAPI/Services/StudentService.cs
@@ -0,0 +1,26 @@
+﻿using GradeTrackerWebAPI.Data;
+using GradeTrackerWebAPI.Helpers;
+using GradeTrackerWebAPI.Models;
+using GradeTrackerWebAPI.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GradeTrackerWebAPI.Services;
+
+public class StudentService(GradeTrackerContext context) : EntityService<StudentEntity>(context), IEntityService<StudentEntity>
+{
+    public async override Task<List<StudentEntity>> GetAll(bool includeAllProperties = false)
+    {
+        if (!includeAllProperties)
+            return await base.GetAll(includeAllProperties);
+
+        return await _context.Set<StudentEntity>()
+            .IncludeAll()
+            .ToListAsync();
+    }
+
+    public override async Task<StudentEntity?> Get(int id)
+        => await _context.Set<StudentEntity>()
+        .Where(x => x.Id == id)
+        .IncludeAll()
+        .FirstOrDefaultAsync();
+}
