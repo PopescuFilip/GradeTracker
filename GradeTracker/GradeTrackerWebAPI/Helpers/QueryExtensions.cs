befe50c02a21bda050310970adebeff811fc296d
FilipPopescu <filippopescu18@gmail.com>
2025-03-27 22:16:57 +0200

    SCRUM-8 updated QueryExtensions

--- GradeTracker/GradeTrackerWebAPI/Helpers/QueryExtensions.cs
+++ GradeTracker/GradeTrackerWebAPI/Helpers/QueryExtensions.cs
@@ -9,4 +9,15 @@ public static class QueryExtensions
         => query
             .Include(c => c.Students)
             .Include(c => c.Assignments);
+
+    public static IQueryable<StudentEntity> IncludeAll(this IQueryable<StudentEntity> query)
+        => query
+            .Include(c => c.Subjects)
+            .Include(c => c.Grades);
+
+    public static IQueryable<GradeEntity> IncludeAll(this IQueryable<GradeEntity> query)
+        => query
+            .Include(c => c.Assignment)
+            .Include(c => c.Assignment.Subject)
+            .Include(c => c.Student);
 }
