2d76ae61e75880a79c1c78c1ec74951443d6975f
IoanaMitrofan <ioana.mitrofan@student.unitbv>
2025-03-31 19:14:27 +0300

    SCRUM-26 Added models for assignment, grade, and subject

---
+++ GradeTracker/GradeTracker/Models/Assignment.cs
@@ -0,0 +1,9 @@
+﻿namespace GradeTracker.Models
+{
+    public class Assignment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public int SubjectId { get; set; }
+    }
+}
