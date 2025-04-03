db854a6999385f3b16745a51ef961a2c2813e8eb
FilipPopescu <filippopescu18@gmail.com>
2025-04-03 20:29:49 +0300

    SCRUM-18 Added general TeacherPage functionality infrastructure

--- GradeTracker/GradeTracker/Components/Pages/TeacherPage.razor.cs
+++ GradeTracker/GradeTracker/Components/Pages/TeacherPage.razor.cs
@@ -1,5 +1,89 @@
+using GradeTracker.Helpers;
+using GradeTracker.Models;
+using GradeTracker.Services.Interfaces;
+using GradeTracker.ViewModels;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Radzen.Blazor;
+using System.Threading.Tasks;
+
 namespace GradeTracker.Components.Pages;
 
 public partial class TeacherPage
 {
+    [Inject]
+    public AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+
+    [Inject]
+    public IGradeHelper GradeHelper { get; set; }
+
+    [Inject]
+    public IGradeService GradeService { get; set; }
+
+    [Inject]
+    public ISubjectService SubjectService { get; set; }
+
+    private bool IsCreateGradeModalVisible { get; set; }
+
+    private List<GradeViewModel> grades;
+    private RadzenDataGrid<GradeViewModel> gradeGrid;
+    private int subjectId;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+        var teacherId = int.Parse(authState.User.Identity.Name);
+        var subject = await SubjectService.GetSubjectForTeacher(teacherId);
+        subjectId = subject.Id;
+
+        grades = await GradeHelper.GetGradesForSubject(subject.Id);
+    }
+
+    private async void HandleGradeCreated()
+    {
+        grades = await GradeHelper.GetGradesForSubject(subjectId);
+
+        await gradeGrid.Reload();
+        CloseCreateTaskModal();
+    }
+
+    private void HandleCreateModelVisibility(bool isVisible)
+    {
+        IsCreateGradeModalVisible = isVisible;
+        StateHasChanged();
+    }
+
+    private void CloseCreateTaskModal()
+    {
+        IsCreateGradeModalVisible = false;
+    }
+
+    private void ShowCreateTaskModal()
+    {
+        IsCreateGradeModalVisible = true;
+    }
+
+    private async Task EditRow(GradeViewModel grade)
+    {
+        await gradeGrid.EditRow(grade);
+    }
+
+    private async Task SaveRow(GradeViewModel grade)
+    {
+        await GradeService.UpdateGrade(grade.Id, grade.Grade);
+        await gradeGrid.UpdateRow(grade);
+    }
+
+    private void CancelEdit(GradeViewModel grade)
+    {
+        gradeGrid.CancelEditRow(grade);
+    }
+
+    private async Task DeleteRow(GradeViewModel grade)
+    {
+        grades.Remove(grade);
+        await GradeService.DeleteGrade(grade.Id);
+
+        await gradeGrid.Reload();
+    }
 }
