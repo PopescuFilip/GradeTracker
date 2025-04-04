d802489e2fe289e39d2d1f4ce0d05d08234c3c24
IoanaMitrofan <ioana.mitrofan@student.unitbv>
2025-04-04 10:57:26 +0300

    SCRUM-29 Added average for grades

--- GradeTracker/GradeTracker/Models/SubjectGradesList.cs
+++ GradeTracker/GradeTracker/Models/SubjectGradesList.cs
@@ -4,5 +4,6 @@
     {
         public string SubjectName { get; set; } = string.Empty;
         public List<GradeEntity> Grades { get; set; } = new List<GradeEntity>();
+        public double AverageSubjectGrades { get; set; } = 0;
     }
 }
